cbfd185d027fd71a2cab38a83e819c7061e02ded
RGV <ricardo.guzman.velasco@outlook.com>
2022-09-24 14:50:59 +0200

    feat(connect4): drop token in board

--- Assets/Connect4/Runtime/Domain/Board.cs
+++ Assets/Connect4/Runtime/Domain/Board.cs
@@ -1,7 +1,24 @@
+using System;
+using static RGV.DesignByContract.Runtime.Contract;
+
 namespace Connect4.Runtime.Domain
 {
     public class Board
     {
+        (int rows, int columns) size;
+
+        public event Action<int> TokenDroppedInRow;
         
+        public Board(int rows, int columns)
+        {
+            size = (rows, columns);
+        }
+
+        public void DropInRow(int onebasedRow)
+        {
+            Require(onebasedRow).Between(1, size.rows);
+            
+            TokenDroppedInRow?.Invoke(onebasedRow);
+        }
     }
 }
