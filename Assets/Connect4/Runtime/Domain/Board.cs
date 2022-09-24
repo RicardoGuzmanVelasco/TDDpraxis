5e6c9b7b48eb7e38642149736d9bae7cb404076d
RGV <ricardo.guzman.velasco@outlook.com>
2022-09-24 14:58:11 +0200

    feat(connect4): drop token only in not-full columns

--- Assets/Connect4/Runtime/Domain/Board.cs
+++ Assets/Connect4/Runtime/Domain/Board.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using static RGV.DesignByContract.Runtime.Contract;
 
 namespace Connect4.Runtime.Domain
@@ -6,19 +7,26 @@ namespace Connect4.Runtime.Domain
     public class Board
     {
         (int rows, int columns) size;
+        Dictionary<int, int> tokensPerColumn = new();
 
-        public event Action<int> TokenDroppedInRow;
+        public event Action<int> TokenDroppedInColumn;
         
         public Board(int rows, int columns)
         {
             size = (rows, columns);
         }
 
-        public void DropInRow(int onebasedRow)
+        public void DropInColumn(int onebasedRow)
         {
             Require(onebasedRow).Between(1, size.rows);
+
+            if(tokensPerColumn.ContainsKey(onebasedRow))
+                tokensPerColumn[onebasedRow]++;
+            else
+                tokensPerColumn[onebasedRow] = 1;
             
-            TokenDroppedInRow?.Invoke(onebasedRow);
+            if(tokensPerColumn[onebasedRow] < size.rows)
+                TokenDroppedInColumn?.Invoke(onebasedRow);
         }
     }
 }
--- Assets/Connect4/Tests/LogicTests.cs
+++ Assets/Connect4/Tests/LogicTests.cs
@@ -1,3 +1,5 @@
+using Connect4.Runtime.Domain;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Connect4.Tests
@@ -5,9 +7,28 @@ namespace Connect4.Tests
     public class LogicTests
     {
         [Test]
-        public void METHOD()
+        public void Drop_aToken_inBoard()
         {
+            var sut = new Board(rows: 6, columns: 7);
+            var spy = sut.Monitor();
+
+            sut.DropInColumn(4);
+
+            spy.Should()
+                .Raise(nameof(Board.TokenDroppedInColumn))
+                .WithArgs<int>(r => r == 4);
+        }
+
+        [Test]
+        public void CannotDrop_aToken_inFullColumn()
+        {
+            var sut = new Board(rows: 2, columns: 1);
+            sut.DropInColumn(1);
+            var spy = sut.Monitor();
             
+            sut.DropInColumn(1);
+
+            spy.Should().NotRaise(nameof(Board.TokenDroppedInColumn));
         }
     }
 }
