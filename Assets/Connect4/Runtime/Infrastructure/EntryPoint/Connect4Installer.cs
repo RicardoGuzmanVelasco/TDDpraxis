b8874df402cfa6fac92f4a15c6b724f3cc485a79
RGV <ricardo.guzman.velasco@outlook.com>
2022-09-25 17:11:43 +0200

    build: create dependency injection installer

---
+++ Assets/Connect4/Runtime/Infrastructure/EntryPoint/Connect4Installer.cs
@@ -0,0 +1,32 @@
+using Connect4.Runtime.Application;
+using Connect4.Runtime.Presentation;
+using Zenject;
+
+namespace Connect4.Runtime.Infrastructure.EntryPoint
+{
+    public class Connect4Installer : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            InstallDomain();
+            InstallApplication();
+            InstallPresentation();
+        }
+
+        void InstallDomain()
+        {
+            Container.Bind<Domain.Board>().FromInstance(new Domain.Board(6, 7));
+            Container.Bind<Domain.Cursor>().FromInstance(new Domain.Cursor(6));
+        }
+        
+        void InstallApplication()
+        {
+            Container.BindInterfacesAndSelfTo<TokenDrop>().FromNew().AsSingle();
+        }
+        
+        void InstallPresentation()
+        {
+            Container.BindInterfacesAndSelfTo<TestPlaceholderView>().FromNewComponentOnNewGameObject().AsSingle();
+        }
+    }
+}
