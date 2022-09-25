dc5ce1c601be65f97754986c8147df78547612ce
RGV <ricardo.guzman.velasco@outlook.com>
2022-09-25 16:47:43 +0200

    feat: receive input to commit drops

---
+++ Assets/Connect4/Runtime/Presentation/CommitDropInput.cs
@@ -0,0 +1,17 @@
+using Connect4.Runtime.Application;
+using UnityEngine;
+using Zenject;
+
+namespace Connect4.Runtime.Presentation
+{
+    public class CommitDropInput : MonoBehaviour
+    {
+        [Inject] TokenDrop controller;
+        
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                controller.InCurrentColumn();
+        }
+    }
+}
