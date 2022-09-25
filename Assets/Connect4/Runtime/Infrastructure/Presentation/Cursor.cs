6dd3b252de82b9027ad3e8cd1dbae8c3273bedaf
RGV <ricardo.guzman.velasco@outlook.com>
2022-09-25 18:22:16 +0200

    build: import DOTween as local package

--- Assets/Connect4/Runtime/Infrastructure/Presentation/Cursor.cs
+++ Assets/Connect4/Runtime/Infrastructure/Presentation/Cursor.cs
@@ -1,41 +1,50 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Connect4.Runtime.Application;
+using DG.Tweening;
 using UnityEngine;
-using Zenject;
 
 namespace Connect4.Runtime.Infrastructure.Presentation
 {
     public class Cursor : MonoBehaviour, CursorView
     {
-        [Inject] CursorMovement controller;
-        
-        void Update()
-        {
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
-                controller.Left();
-            else if(Input.GetKeyDown(KeyCode.RightArrow))
-                controller.Right();
-        }
-
         public Task MoveTo(int column)
         {
             var snapToThis = FindObjectsOfType<TokenSlot>().First(s => s.IsInColumn(column));
-            
-            var target = new Vector3
-            (
-                snapToThis.transform.position.x,
-                transform.position.y,
-                transform.position.z
-            );
-            transform.position = target;
+            transform.DOMoveX(snapToThis.transform.position.x, .3f).SetEase(Ease.OutBounce);
 
             return Task.CompletedTask;
         }
 
-        public async Task InvalidDirection(Vector2Int direction)
+        public Task InvalidDirection(Vector2Int direction)
         {
-            Debug.Log("Invalid direction " + direction);
+            WarnRedColor();
+            MagnetSnapTowardsInvalidDirection(direction);
+
+            return Task.CompletedTask;
+        }
+
+        void MagnetSnapTowardsInvalidDirection(Vector2Int direction)
+        {
+            transform.DOShakePosition
+            (
+                duration: .15f,
+                strength: new Vector3(direction.x * .05f, 0),
+                vibrato: 25,
+                randomness: 0,
+                snapping: false,
+                fadeOut: false
+            );
+        }
+
+        void WarnRedColor()
+        {
+            var renderer = GetComponent<SpriteRenderer>();
+            renderer.DOColor
+            (
+                endValue: Color.red,
+                duration: .15f
+            ).SetLoops(2, LoopType.Yoyo);
         }
     }
 }
